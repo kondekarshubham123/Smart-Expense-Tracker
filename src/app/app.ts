368145dfefd6ca0f212b98798add5b080fabaef7
Shubham Kondekar <kondekarshubham123@gmail.com>
2025-12-27 12:09:42 +0530

    initial structure has been added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, signal, inject } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { AuthService } from './core/services/auth';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule
+
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('smart-expense-tracker');
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        console.log('User logged out successfully');
+      },
+      error: (err) => {
+        console.error('Error during logout:', err);
+      }
+    })
+  };
+
+  get isAuthPage(): boolean { 
+    const url = this.router.url;
+    return url.includes('login') || url.includes('register');
+  }
 }
--- src/app/features/dashboard/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard/dashboard.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  authService = inject(AuthService);
 
+  get userName(): string {
+    return this.authService.currentUser()?.displayName || 'User';
+  }
+
+  get userEmail(): string | null {
+    return this.authService.currentUser()?.email || null;
+  }
 }
